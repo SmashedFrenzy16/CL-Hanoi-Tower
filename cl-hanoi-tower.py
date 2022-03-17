2b9702864ff179ba82b72032e455121c214ddfd2
SmashedFrenzy16 <68993968+SmashedFrenzy16@users.noreply.github.com>
2022-03-17 16:42:22 +0000

    Update cl-hanoi-tower.py

--- cl-hanoi-tower.py
+++ cl-hanoi-tower.py
@@ -3,4 +3,14 @@ def towerOfHanoi(n, fromRod, toRod, auxRod):
   if n ==0:
     
     return
+  
+  towerOfHanoi(n-1, fromRod, auxRod, toRod)
+  
+  print("Move disk",n,"From rod",fromRod,"to rod",toRod)
+  
+  towerOfHanoi(n-1, auxRod, fromRod, toRod)
+  
+  n = 6
+
+  towerOfHanoi(n, "A", "C", "B")
 
